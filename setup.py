7502ed41cc3547bd2d35bf41d3e5560d9172f813
sarar04 <sarar04@163.com>
2016-12-27 16:14:09 +0800

    update setup.py
    Signed-off-by: sarar04 <sarar04@163.com>

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'djangoapigateway',
-    version = '0.0.4',
+    name = 'django-api-gateway',
+    version = '0.0.1',
     keywords = ('django', 'apigateway', 'dataproxy'),
     description = 'apigateway for django',
     license = 'MIT License',
