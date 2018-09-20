8acd1e1a9747183c1aad3c8453140b78b30fa7e0
Tianhu Yang <tianhuyang@gmail.com>
2018-09-20 12:48:09 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-api-gateway',
-    version = '0.0.5',
-    keywords = ('django', 'apigateway', 'dataproxy'),
-    description = 'apigateway for django',
-    license = 'MIT License',
+    name='django-api-gateway',
+    version='0.0.5',
+    keywords=('django', 'apigateway', 'dataproxy'),
+    description='apigateway for django',
+    license='MIT License',
 
-    url = 'https://github.com/ecidi/djangoapigateway',
-    author = 'ecidi',
-    author_email = 'sarar04@163.com',
+    url='https://github.com/tianhuyang/djangoapigateway',
+    author='tianhuyang',
+    author_email='tianhuyang@gmail.com',
 
-    packages = find_packages(),
-    include_package_data = True,
-    platforms = 'unix like',
-    install_requires = []
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='unix like',
+    install_requires=[]
 )
